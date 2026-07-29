161717e2a4e03af8595a7e424c6883bd3efe1ab8
Lafu666 <3900116132@qq.com>
2026-07-29 14:47:20 +0800

    Implement AppDelegate with initial view controller

---
+++ SwiftSources/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = WebViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        nav.navigationBar.barStyle = .black
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
